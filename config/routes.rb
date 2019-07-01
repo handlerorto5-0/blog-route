52e46cc6c5fa6a9fb2393249876cbb5472646250
kv-2 <str_token@mail.ru>
2019-07-01 18:17:44 +0300

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
 
-  # to render posts by date any to localhost:3000/2010/10/01, moth and day not necessarily
+  # to render posts by date any to localhost:3000/2010/10/01(month and day not necessarily)
+  # put year,month,day to params array, it can read out parameters from the URL
   get ':year(/:month(/:day))', to: 'posts#index'
 
 end

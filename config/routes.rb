84f90c4fcf3b7acaf4a8a8f8e751368b6d59132c
kv-2 <str_token@mail.ru>
2019-07-01 18:25:27 +0300

    add constraints to toutes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # to render posts by date any to localhost:3000/2010/10/01(month and day not necessarily)
   # put year,month,day to params array, it can read out parameters from the URL
-  get ':year(/:month(/:day))', to: 'posts#index'
+  # constraints define the content of the URL
+  get ':year(/:month(/:day))', to: 'posts#index', 
+  constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{2}/ }
 
 end

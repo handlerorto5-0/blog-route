4a90e7752371feac0f523fec2a8708f7b8a23ca6
kv-2 <str_token@mail.ru>
2019-07-01 16:44:28 +0300

    add optional parameters to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # to render posts by date any to localhost:3000/2010/10/01, moth and day not necessarily
+  get ':year(/:month(/:day))', to: 'posts#index'
+
 end

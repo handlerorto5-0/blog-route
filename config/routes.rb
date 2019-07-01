7b554ff5ec0f831f19d5cf083ed62e73d598023a
kv-2 <str_token@mail.ru>
2019-07-01 18:33:10 +0300

    add redirect ro routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   resources :posts
 
+  #reditect to invdlid URL
+  get ':year/:month/:day', to: redirect("/%{year}/0%{month}/0%{day}"),
+  constraints: { year: /\d{4}/, month: /\d{1}/, day: /\d{1}/ }
+  get ':year/:month/:day', to: redirect("/%{year}/0%{month}/%{day}"),
+  constraints: { year: /\d{4}/, month: /\d{1}/, day: /\d{2}/ }
+  get ':year/:month/:day', to: redirect("/%{year}/%{month}/0%{day}"),
+  constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{1}/ }
+  get ':year/:month', to: redirect("/%{year}/0%{month}"),
+  constraints: { year: /\d{4}/, month: /\d{1}/ }
+
   # to render posts by date any to localhost:3000/2010/10/01(month and day not necessarily)
   # put year,month,day to params array, it can read out parameters from the URL
   # constraints define the content of the URL
